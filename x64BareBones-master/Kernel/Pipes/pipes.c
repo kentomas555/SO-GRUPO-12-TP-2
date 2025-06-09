fd3bc514f1b606e3a5e4ffca8e65efe277c1ce93
romanberruti <rberruti@itba.edu.ar>
2025-06-08 23:06:23 -0300

    started pipes and changes in semaphores
    Co-Authored-By: HwaPyoungKim <119864357+HwaPyoungKim@users.noreply.github.com>
    Co-Authored-By: kentomas555 <152535534+kentomas555@users.noreply.github.com>

--- x64BareBones-master/Kernel/Pipes/pipes.c
+++ x64BareBones-master/Kernel/Pipes/pipes.c
@@ -1,1 +1,21 @@
-#include "pipes.h"
+#include "../include/pipes.h"
+#include "../include/MemoryManager.h"
+
+typedef struct PipeCDT {
+    char buffer[PIPE_BUFFER_SIZE];
+    int readPos;
+    int writePos;
+    Pid writerPID;
+    Pid readerPID;
+    int readFD;
+    int writeFD;
+    sem_t * semRead;
+    sem_t * semWrite;
+    sem_t * mutex;
+
+} PipeCDT;
+
+PipeCDT * pipeTable[MAX_PIPES];
+
+
+
--- x64BareBones-master/Kernel/include/pipes.h
+++ x64BareBones-master/Kernel/include/pipes.h
@@ -6,6 +6,7 @@
 #include "process.h"
 
 #define MAX_PIPES 20
+#define PIPE_BUFFER_SIZE 128
 
 uint64_t createPipe(char * pipeName);
 
